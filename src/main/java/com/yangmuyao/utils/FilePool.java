bf8ffb4e8e440fd9d66b2af1cf33ff152a3625b4
XD.Yang <xd.yang@yangmuyao.com>
2016-06-15 18:39:52 +0800

    add filelist cache

--- src/main/java/com/yangmuyao/utils/FilePool.java
+++ src/main/java/com/yangmuyao/utils/FilePool.java
@@ -1,10 +1,14 @@
 package com.yangmuyao.utils;
 
+import sun.misc.Queue;
+
 import java.io.File;
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * Created by fengyuyangchen on 16/6/14.
@@ -15,6 +19,8 @@ public class FilePool  implements Serializable{
     public      static Map<String ,CacheFile>    getAllFiles(){
         return filepool;
     }
+
+    public      static ConcurrentLinkedQueue<String>  fileHistory = new ConcurrentLinkedQueue<String>();
 
     /**
      *
@@ -33,7 +39,7 @@ public class FilePool  implements Serializable{
             CacheFile               cacheFile   = new CacheFile(uuid);
 
             filepool.put(key,cacheFile);
-
+            fileHistory.add(uuid);
             return cacheFile;
         }else {
             return filepool.get(key);
