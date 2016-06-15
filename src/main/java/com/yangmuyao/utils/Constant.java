8ff088cc50fc1124fc0099563571e5b0afe4c5c3
风雨扬尘 <fengyuyangchen@fengyuyangchendeMac-mini.local>
2016-06-15 17:34:39 +0800

    BI

---
+++ src/main/java/com/yangmuyao/utils/Constant.java
@@ -0,0 +1,7 @@
+package com.yangmuyao.utils;
+
+/**
+ * Created by fengyuyangchen on 16/6/15.
+ */
+public class Constant {
+}
---
+++ src/main/java/com/yangmuyao/utils/FileProcessTask.java
@@ -0,0 +1,7 @@
+package com.yangmuyao.utils;
+
+/**
+ * Created by fengyuyangchen on 16/6/14.
+ */
+public class FileProcessTask {
+}
