2ca5e730b2b85a9706c79488e9ed0feb4c1d5a69
XD.Yang <xd.yang@yangmuyao.com>
2016-06-16 13:32:13 +0800

    定时器Task,定时将文件列表序列化至硬盘

--- src/main/java/com/yangmuyao/utils/CacheTask.java
+++ src/main/java/com/yangmuyao/utils/CacheTask.java
@@ -1,7 +1,25 @@
 package com.yangmuyao.utils;
 
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.util.TimerTask;
+
 /**
  * Created by fengyuyangchen on 16/6/16.
  */
-public class CacheTask {
+public class CacheTask extends TimerTask{
+
+    /**
+     * 定时执行的任务,缓存文件列表
+     */
+    public void run(){
+        try {
+            ObjectOutputStream objectout = new ObjectOutputStream(new FileOutputStream(Constant.CacheFile));
+            objectout.writeObject( FilePool.fileHistory );
+            objectout.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
 }
