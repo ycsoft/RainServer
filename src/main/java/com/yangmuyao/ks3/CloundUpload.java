a41852b2a35a85054f8dff5b370968472b68c75d
XD.Yang <xd.yang@yangmuyao.com>
2016-06-16 13:28:34 +0800

    文件上传线程，在线程池中调度

--- src/main/java/com/yangmuyao/ks3/CloundUpload.java
+++ src/main/java/com/yangmuyao/ks3/CloundUpload.java
@@ -1,7 +1,38 @@
 package com.yangmuyao.ks3;
 
+import com.yangmuyao.utils.FilePool;
+
+import java.io.File;
+
 /**
  * Created by fengyuyangchen on 16/6/16.
  */
-public class CloundUpload {
+
+
+
+public class CloundUpload implements Runnable {
+
+    private String filename = null;
+
+    /**
+     *  在构造函数中指定该线程需要处理的文件
+     * @param filename
+     */
+    public CloundUpload(String filename){
+
+            this.filename = filename;
+    }
+
+    /**
+     *将文件上传至KS3,如果上传成功,则删除该文件
+     */
+    public void run(){
+        File f = new File(filename);
+        KS3Client client = new KS3Client(KS3Client.BEIJING);
+        client.setBucket("citrusjoy.matrix.storage");
+        if ( client.putFile("Garfield/session/" + f.getName(),this.filename) ){
+            f.delete();
+            FilePool.fileHistory.remove(filename);
+        }
+    }
 }
